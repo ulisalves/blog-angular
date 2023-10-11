b4fe6a62c8aa4d3b2881087087f78a6093b405a9
Ulisses Alves de Melo - PrestServ <ulisses.alves.prestserv@petrobras.com.br>
2023-10-11 17:57:03 -0300

    alterações iniciais do blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Plantas Ornamentais para se ter em Casa",
+    "description": "São plantas cultivada por sua beleza e são muito usadas na arquitetura de interiores e no paisagismo de espaços externos. Há indícios de que, desde os primórdios da humanidade, algumas espécies, como o lírio-branco (Lilium candidum), eram cultivadas para esse fim.",
+    "photoCover":"https://entendaantes.com.br/wp-content/uploads/2020/10/plantas-ornamentais-3-600x338-1.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Zamioculca",
+    "description": "É uma folhagem ornamental podendo chegar a 60cm, indicada para os que são mais desligados, pois não precisa de atenção direta.", 
+    "photoCover":"https://www.agromania.com.br/img/2018/01/blog/5246/zamioculca-agromania.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Onze horas",
+    "description": "Quando o sol fica mais forte perto do almoço, os botões de flores começam a se abrir. As '11 horas' são plantas muito fáceis de cuidar indicada para iniciantes.",
+    "photoCover":"https://www.agromania.com.br/img/2018/01/blog/5247/onze-horas-agromania.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Palmeira-Ráfia",
+    "description": "É uma planta bem chique, tem um ar de oriental. Pode chegar até 4 metros de altura, e é ideal para design de interiores. Gosta de bastante água e luz.",
+    "photoCover":"https://www.agromania.com.br/img/2018/01/blog/5243/palmeira-rafia-agromania.jpg"
+  }
+
+]
